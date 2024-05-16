c25959428b97c8c99eada77fd5cda64a80bcb0a3
erlin <85721014+erlincnd@users.noreply.github.com>
2024-05-17 00:56:54 +0700

    Update 6-About.py

--- pages/6-About.py
+++ pages/6-About.py
@@ -24,3 +24,6 @@ with c3:
     st.markdown('<strong><p style="color:green;">Dosen Pembimbing II.</p></strong>', unsafe_allow_html=True)
     st.write ("Dr. Ir. Elviawaty Muisa Zamzami S.T., M.T., MM")
     st.write ("NIP 197007162005012002")
+
+if st.button("Kembali ke Menu utama"):
+    st.switch_page("main.py")
