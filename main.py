80a2ab7298dfd0f6a6e1aa38450f677e2374a14b
erlin <85721014+erlincnd@users.noreply.github.com>
2024-05-17 00:52:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.subheader("Explore various eyeglass shapes according to your face shape for a
 st.image('logo glasses-on 2.png', caption=None, width=300, )
 st.write ("Ketahui bentuk wajah kamu terlebih dahulu")
 if st.button("Mulai"):
-    st.switch_page("pages/upload.py")
+    st.switch_page("pages/2-Upload.py")
