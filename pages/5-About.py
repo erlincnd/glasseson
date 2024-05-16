69ebec8e719fab5302380ffd249926822bdfd17d
erlin <85721014+erlincnd@users.noreply.github.com>
2024-05-17 00:48:12 +0700

    Add files via upload

---
+++ pages/5-About.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.title("Glasses On")
+st.image('logo glasses-on 2.png', caption=None, width=300, )
+
+st.write ("Website ini merupakan sistem yang dibuat untuk menyelesaikan tugas akhir pendidikan S-1 Ilmu Komputer di Universitas Sumatera Utara.")
+st.write ("dengan judul : ")
+st.markdown('<strong><h3 style="color:brown;" align="center">Penerapan Convolutional Neural Network dan Evaluation Based on Distance from Average Solution dalam Sistem Cerdas Kesesuaian Frame Kacamata dengan Identifikasi Bentuk Wajah</h3></strong>', unsafe_allow_html=True)
+
+
+c1,c2,c3 = st.columns(3)
+
+with c1:
+    st.markdown('<strong><p style="color:brown;">Penulis.</p></strong>', unsafe_allow_html=True)
+    st.write ("Erlin Cindini Manullang")
+    st.write ("NIM 201401047")
+
+with c2:
+    st.markdown('<strong><p style="color:brown;">Dosen Pembimbing I.</p></strong>', unsafe_allow_html=True)
+    st.write ("Dewi Sartika Br. Ginting, S.Kom., M.Kom.")
+    st.write ("NIP 199005042019032023")
+
+with c3:
+    st.markdown('<strong><p style="color:brown;">Dosen Pembimbing II.</p></strong>', unsafe_allow_html=True)
+    st.write ("Dr. Ir. Elviawaty Muisa Zamzami S.T., M.T., MM")
+    st.write ("NIP 197007162005012002")
