336a9ca3f39baeace27a8181e3c1c4ee4f360d13
erlin <85721014+erlincnd@users.noreply.github.com>
2024-05-17 01:31:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="Glasses On", page_icon="👓")
 st.title("Glasses On")
 st.subheader("Explore various eyeglass shapes according to your face shape for a visually more attractive appearance...")
 st.image('logo glasses-on 2.png', caption=None, width=300, )
