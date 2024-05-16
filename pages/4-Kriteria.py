488aa089fe4a80b6b5b2ed475804a3a29ba5a190
erlin <85721014+erlincnd@users.noreply.github.com>
2024-05-17 01:31:57 +0700

    Update 4-Kriteria.py

--- pages/4-Kriteria.py
+++ pages/4-Kriteria.py
@@ -2,6 +2,7 @@ import streamlit as st
 from PIL import Image
 import io
 
+st.set_page_config(page_title="Glasses On", page_icon="👓")
 st.title("Glasses On")
 st.image('logo glasses-on 2.png', caption=None, width=200, )
 wajah = st.text_input("Bentuk Wajah: ",st.session_state['bentuk'])
